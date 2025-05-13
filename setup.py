9b737c4085a70fe622e9cdfe68cdd71e36ce3e89
Sanjib Basak <info@studyexam.ai>
2025-05-13 01:54:46 +0000

    2nd commit

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ setup(
 
     include_package_data=True,
     install_requires=load_requirements('requirements/base.in'),
-    python_requires=">=3.12",
+    python_requires=">=3.11",
     zip_safe=False,
     keywords='Python edx',
     classifiers=[
